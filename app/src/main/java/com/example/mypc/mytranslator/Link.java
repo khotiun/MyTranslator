852617edd3e7dc95628b20d9659e13dbe60bcd17
vadim khotiun <hotun.vadim@gmail.com>
2017-04-21 22:32:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mypc/mytranslator/Link.java
@@ -0,0 +1,21 @@
+package com.example.mypc.mytranslator;
+
+import java.util.Map;
+
+import retrofit.Call;
+import retrofit.http.FieldMap;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+/**
+ * Created by MyPc on 21.04.2017.
+ */
+
+public interface Link {
+    //content://authority/path
+//   https://translate.yandex.net/api/v1.5/tr.json/translate
+// api/v1.5/tr.json/translate
+    @FormUrlEncoded
+    @POST("/api/v1.5/tr.json/translate")
+    Call<Object> translate(@FieldMap Map<String, String > map);
+}
