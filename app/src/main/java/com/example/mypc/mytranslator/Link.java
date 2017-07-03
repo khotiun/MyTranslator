ab44bb41986e61881837cac7c9f70763987081e6
Vadym Khotiun <hotun.vadim@gmail.com>
2017-07-03 08:47:55 +0300

    Delete folder .idea

--- app/src/main/java/com/example/mypc/mytranslator/Link.java
+++ app/src/main/java/com/example/mypc/mytranslator/Link.java
@@ -11,11 +11,12 @@ import retrofit.http.POST;
  * Created by MyPc on 21.04.2017.
  */
 
-public interface Link {
+public interface Link {//интерфейс для управления адресом, используя команды GET, POST и т.д.
     //content://authority/path
 //   https://translate.yandex.net/api/v1.5/tr.json/translate
 // api/v1.5/tr.json/translate
-    @FormUrlEncoded
-    @POST("/api/v1.5/tr.json/translate")
-    Call<Object> translate(@FieldMap Map<String, String > map);
+    @FormUrlEncoded//При использовании пары "имя/значение" в POST-запросах применяется аннотации @FormUrlEncoded и @FieldMap:
+    @POST("/api/v1.5/tr.json/translate")//ссылка куда отправляется запрос
+    Call<Object> translate(@FieldMap Map<String, String > map);// Call<Object> - то что получаем назад.
+    //Запросы размещаются в обобщённом классе Call с указанием желаемого типа.
 }
--- app/src/main/java/com/example/mypc/mytranslator/MainActivity.java
+++ app/src/main/java/com/example/mypc/mytranslator/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -23,16 +22,16 @@ import retrofit.Retrofit;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText text;
-    TextView translated;
-    Button translateBtn;
+    EditText translated;
+    Button btnEnRu, btnRuEn, btnPlRu, btnRuPl, btnFrRu, btnRuFr, btnDeRu, btnRuDe, btnZhRu, btnRuZh;
 
     final private String URL = "https://translate.yandex.net";
     final private String KEY = "trnsl.1.1.20160719T224220Z.24f42814f637f644.dc9cbf9b0dec16722dd04d18cbbac6d6d1974d74";
     private Gson gson = new GsonBuilder()
             .create();
-    private Retrofit retrofit = new Retrofit.Builder()
+    private Retrofit retrofit = new Retrofit.Builder()//Retrofit - класс для обработки результатов. Ему нужно указать базовый адрес в методе baseUrl()
             .addConverterFactory(GsonConverterFactory.create(gson))//
-            .baseUrl(URL)
+            .baseUrl(URL)//базовый url
             .build();
     private Link intf = retrofit.create(Link.class);
 
@@ -40,22 +39,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();//Что это?
         StrictMode.setThreadPolicy(policy);
         initialize();
     }
 
     private void initialize() {
-        translateBtn = (Button) findViewById(R.id.translate_btn);
-        translateBtn.setOnClickListener(this);
-        translated = (TextView) findViewById(R.id.translated);
+        btnEnRu = (Button) findViewById(R.id.btn_en_ru);
+        btnEnRu.setOnClickListener(this);
+        btnPlRu = (Button) findViewById(R.id.btn_pl_ru);
+        btnPlRu.setOnClickListener(this);
+        btnFrRu = (Button) findViewById(R.id.btn_fr_ru);
+        btnFrRu.setOnClickListener(this);
+        btnDeRu = (Button) findViewById(R.id.btn_de_ru);
+        btnDeRu.setOnClickListener(this);
+        btnZhRu = (Button) findViewById(R.id.btn_zh_ru);
+        btnZhRu.setOnClickListener(this);
+
+        btnRuEn = (Button) findViewById(R.id.btn_ru_en);
+        btnRuEn.setOnClickListener(this);
+        btnRuPl = (Button) findViewById(R.id.btn_ru_pl);
+        btnRuPl.setOnClickListener(this);
+        btnRuFr = (Button) findViewById(R.id.btn_ru_fr);
+        btnRuFr.setOnClickListener(this);
+        btnRuDe = (Button) findViewById(R.id.btn_ru_de);
+        btnRuDe.setOnClickListener(this);
+        btnRuZh = (Button) findViewById(R.id.btn_ru_zh);
+        btnRuZh.setOnClickListener(this);
+        translated = (EditText) findViewById(R.id.translated);
         text = (EditText) findViewById(R.id.text);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.translate_btn:
+            case R.id.btn_en_ru:
                 Map<String, String> mapjson = new HashMap<String, String>();
                 mapjson.put("key", KEY);
                 mapjson.put("text", text.getText().toString());
@@ -63,20 +81,232 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 Call<Object> call = intf.translate(mapjson);
                 try {
-                    Response<Object> res = call.execute();
-
-                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);
+                    Response<Object> res = call.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
 
                     for (Map.Entry e : map.entrySet()) {
                         if (e.getKey().equals("text")) {
-                            translated.setText(e.getValue().toString());
+                            String str = e.getValue().toString();
+                            translated.setText(str.substring(1, str.length()-1));
                         }
-                        //System.out.println(e.getKey()+" "+e.getValue());
+//                        System.out.println(e.getKey()+" "+e.getValue());
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 break;
+            case R.id.btn_pl_ru:
+                Map<String, String> mapjsonPlRu = new HashMap<String, String>();
+                mapjsonPlRu.put("key", KEY);
+                mapjsonPlRu.put("text", text.getText().toString());
+                mapjsonPlRu.put("lang", "pl-ru");
+
+                Call<Object> callPlRu = intf.translate(mapjsonPlRu);
+                try {
+                    Response<Object> res = callPlRu.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            translated.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_fr_ru:
+                Map<String, String> mapjsonFrRu = new HashMap<String, String>();
+                mapjsonFrRu.put("key", KEY);
+                mapjsonFrRu.put("text", text.getText().toString());
+                mapjsonFrRu.put("lang", "fr-ru");
+
+                Call<Object> callFrRu = intf.translate(mapjsonFrRu);
+                try {
+                    Response<Object> res = callFrRu.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            translated.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_de_ru:
+                Map<String, String> mapjsonDeRu = new HashMap<String, String>();
+                mapjsonDeRu.put("key", KEY);
+                mapjsonDeRu.put("text", text.getText().toString());
+                mapjsonDeRu.put("lang", "de-ru");
+
+                Call<Object> callDeRu = intf.translate(mapjsonDeRu);
+                try {
+                    Response<Object> res = callDeRu.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            translated.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_zh_ru:
+                Map<String, String> mapjsonZhRu = new HashMap<String, String>();
+                mapjsonZhRu.put("key", KEY);
+                mapjsonZhRu.put("text", text.getText().toString());
+                mapjsonZhRu.put("lang", "zh-ru");
+
+                Call<Object> callZhRu = intf.translate(mapjsonZhRu);
+                try {
+                    Response<Object> res = callZhRu.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            translated.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+
+
+            case R.id.btn_ru_en:
+                Map<String, String> mapjsonRuEn = new HashMap<String, String>();
+                mapjsonRuEn.put("key", KEY);
+                mapjsonRuEn.put("text", translated.getText().toString());
+                mapjsonRuEn.put("lang", "ru-en");
+
+                Call<Object> callRuEn = intf.translate(mapjsonRuEn);
+                try {
+                    Response<Object> res = callRuEn.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            text.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_ru_pl:
+                Map<String, String> mapjsonRuPl = new HashMap<String, String>();
+                mapjsonRuPl.put("key", KEY);
+                mapjsonRuPl.put("text", translated.getText().toString());
+                mapjsonRuPl.put("lang", "ru-pl");
+
+                Call<Object> callRuPl = intf.translate(mapjsonRuPl);
+                try {
+                    Response<Object> res = callRuPl.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            text.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_ru_fr:
+                Map<String, String> mapjsonRuFr = new HashMap<String, String>();
+                mapjsonRuFr.put("key", KEY);
+                mapjsonRuFr.put("text", translated.getText().toString());
+                mapjsonRuFr.put("lang", "ru-fr");
+
+                Call<Object> callRuFr = intf.translate(mapjsonRuFr);
+                try {
+                    Response<Object> res = callRuFr.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            text.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_ru_de:
+                Map<String, String> mapjsonRuDe = new HashMap<String, String>();
+                mapjsonRuDe.put("key", KEY);
+                mapjsonRuDe.put("text", translated.getText().toString());
+                mapjsonRuDe.put("lang", "ru-de");
+
+                Call<Object> callRuDe = intf.translate(mapjsonRuDe);
+                try {
+                    Response<Object> res = callRuDe.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            text.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_ru_zh:
+                Map<String, String> mapjsonRuZh = new HashMap<String, String>();
+                mapjsonRuZh.put("key", KEY);
+                mapjsonRuZh.put("text", translated.getText().toString());
+                mapjsonRuZh.put("lang", "ru-zh");
+
+                Call<Object> callRuZh = intf.translate(mapjsonRuZh);
+                try {
+                    Response<Object> res = callRuZh.execute();//Для синхронного запроса используйте метод Call.execute(), для асинхронного - метод Call.enqueue().
+                    //В Response - получаем json обьект
+                    Map<String, String> map = gson.fromJson(res.body().toString(), Map.class);//res.body() - все что пришло с сервера
+
+                    for (Map.Entry e : map.entrySet()) {
+                        if (e.getKey().equals("text")) {
+                            String str = e.getValue().toString();
+                            text.setText(str.substring(1, str.length()-1));
+                        }
+//                        System.out.println(e.getKey()+" "+e.getValue());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+
             default:
                 break;
         }
